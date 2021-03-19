def47000b2f2286de5c28f2788ff51e6f456f8e4
Douglas Basilio dos Santos <basilio.douglas@yahoo.com.br>
2021-03-19 08:41:29 -0300

    Adicionando arquivos de projeto

---
+++ P12-CalculaPoupanca/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 12 - Calcula poupança");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                // 0.36% = 0.0036
+                valorInvestido += valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$ " + valorInvestido);
+                contadorMes++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

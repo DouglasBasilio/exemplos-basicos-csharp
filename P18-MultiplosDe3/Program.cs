26c0250dd3e41c20451d783e6047cce6a40ea1e5
Douglas Basilio dos Santos <basilio.douglas@yahoo.com.br>
2021-03-22 08:16:53 -0300

    desafios

---
+++ P18-MultiplosDe3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P18_MultiplosDe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+
+            for (int i = 3; i < 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P19-Fatorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P19_Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //O fatorial é sempre o produto de números consecutivos inteiros de 1 até o próprio número.
+            //Ex: Fatorial de 4 = 4 x 3 x 2 x 1 = 24 ou fatorial de 4 = 4 x 3! = 24.
+
+            int fatorial = 1;
+            for (int i = 1; i < 11; i++)
+            {
+                fatorial *= i;
+                Console.WriteLine("Fatorial de " + i + " = " + fatorial);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
